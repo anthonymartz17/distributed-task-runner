7c12585827af2e22b53f9de9ea822528c56f5549
antonio <antonio.fr.martinezc@hotmail.com>
2025-07-24 12:05:56 -0400

    implement thread-sace fifo queue

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -3,7 +3,6 @@ package domain
 import "time"
 
 
-
 type Task struct{
 	Id string `json:"id"`
 	Type string `json:"type"`
@@ -11,9 +10,3 @@ type Task struct{
 	CreatedAt time.Time `json:"create_at"`
 }
 
-type Result struct{
-	TaskId string `json:"task_id"`
-	Result interface{} `json:"result"`
-	Error string `json:"error,omitempty"`
-	CompletedAt time.Time `json:"completed_at"`
-}
