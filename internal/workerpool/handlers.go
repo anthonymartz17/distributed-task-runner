a29a9e09b48ee72762884cb07f3a2235473cb341
antonio <antonio.fr.martinezc@hotmail.com>
2025-08-18 11:39:42 -0400

    Ensure long-running or hanging tasks respect context timeouts/deadlines.

--- internal/workerpool/handlers.go
+++ internal/workerpool/handlers.go
@@ -1,62 +1,72 @@
 package workerpool
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"strings"
 	"time"
 
 	"github.com/anthonymartz17/distributed-task-runner/internal/domain"
 )
 
-func HandleWordCount(task *domain.Task) *domain.Result{
+func HandleWordCount(task *domain.Task,ctx context.Context) (*domain.Result,error){
   var text string
-
+	
 	if err:= json.Unmarshal(task.Payload,&text); err != nil{
+		return nil,err
+	}
+  words:=  strings.Fields(text)
+  var count int64  
 
-		return&domain.Result{
-			TaskId: task.Id,
-			Error: err.Error(),
-			CompletedAt: time.Now(),
+	for i:= 0; i < len(words); i++{
+    
+		select{
+		case <- ctx.Done():
+			return nil, errors.New("process canceled")
+
+		default:
+			count++
 		}
 	}
-
-	count:= len(strings.Fields(text))
 
 	return&domain.Result{
 		TaskId: task.Id,
 		Result: count,
 		CompletedAt: time.Now(),
-	}
+	},nil
 
 }
 
-func HandleReverseArrayInt(task *domain.Task) *domain.Result{
+func HandleReverseArrayInt(task *domain.Task, ctx context.Context) (*domain.Result,error){
  
 	var result []int64
 
 	if err:= json.Unmarshal(task.Payload,&result); err != nil{
-		return&domain.Result{
-			TaskId: task.Id,
-			Error: err.Error(),
-			CompletedAt: time.Now(),
-		}
+		return  nil,err
 	}
 
 	l:= 0
 	r:= len(result) - 1
 
 	for l < r{
-   temp:= result[l]
-	 result[l] =  result[r]
-	 result[r] = temp
-	 l++
-	 r--
+
+   select{
+	 case <- ctx.Done():
+		return nil, errors.New("process canceled")
+	default:
+		temp:= result[l]
+		result[l] =  result[r]
+		result[r] = temp
+		l++
+		r--
+	}
 	}
 
 	return&domain.Result{
 		TaskId: task.Id,
 		Result: result,
 		CompletedAt: time.Now(),
-	}
+	},nil
 }
 
